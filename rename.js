b0b5c99ef1674d09189053e581c33b4e0f0b717a
Jayin Ton <tonjayin@gmail.com>
2015-12-14 01:29:42 +0800

    first blood

---
+++ rename.js
@@ -0,0 +1,14 @@
+// 把XXXX-YYYY.md的文件格式转换为YYYY.md
+
+var fs = require('fs')
+var path = require('path')
+
+var QuestionPath = './questions'
+var questionFiles = fs.readdirSync(QuestionPath)
+
+questionFiles.forEach(function(file){
+    var newFileName = file.split('-')[1]
+
+    fs.renameSync(path.join(QuestionPath, file), path.join(QuestionPath, newFileName))
+    console.log('%s -> %s',path.join(QuestionPath, file), path.join(QuestionPath, newFileName))
+})
